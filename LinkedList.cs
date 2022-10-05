9a49f5e8ea8ddb8e4117d6d66ae8891cf8cee3b1
Shalom Gar <gar.shalom1@gmail.com>
2022-10-05 19:59:11 +0100

    Add files via upload

---
+++ LinkedList.cs
@@ -0,0 +1,142 @@
+﻿
+public class LinkedList<T>
+{
+
+    private int counter;
+    private Node<T> head;
+    private Node<T> current;
+    private Node<T> prev;
+    
+    
+
+    public LinkedList()
+    {
+        counter = 0;
+        head = null;
+        current = null;
+        prev = null;
+        
+    }
+
+
+
+
+
+    public void Add(T data)
+    {
+        Node<T> myNode = new Node<T>(data);
+        current = head;
+
+        if (head == null)
+        {
+            head = myNode;
+        }
+        else
+        {
+            
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            current.next = myNode;
+        }
+
+        counter++;
+    }
+
+    public bool Remove(T data)
+    {
+        current = head;
+        Node<T> myNode = new Node<T>(data);
+        
+        if (head.data.Equals(myNode.data))
+        {
+            current = head.next;
+            head = current;
+            return true;
+        }
+        else
+        {
+            while (current != null)
+            {
+                if (current.data.Equals(myNode.data))
+                {
+                    prev.next = current.next;
+                    current = prev;
+                    return true;
+                }
+                else
+                {
+                    prev = current;
+                    current = current.next;
+                }
+            }
+        }
+
+        counter--;
+
+        return false;
+    }
+
+
+    public bool Check(T data)
+    {
+        var myNode = new Node<T>(data);
+
+        var current = head;
+
+        while (current != null)
+        {
+            if (current.data.Equals(myNode.data))
+            {
+
+                return true;
+            }
+
+            current = current.next;
+        }
+        return false;
+    }
+
+
+    public int Index(T data)
+    {
+        var myNode = new Node<T>(data);
+        var current = head;
+        int index = 0;
+        
+        while (current != null)
+        {
+            if (current.data.Equals(myNode.data))
+            {
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+
+        return -1;
+    }
+    
+
+    public T display()
+    {
+        current = head;
+
+        while (current.next != null)
+        {
+            Console.WriteLine(current.data);
+            current = current.next;
+
+        }
+        if (current.next == null)
+        {
+            Console.WriteLine(current.data);
+        }
+
+        return default;
+        //return current.next.data;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+    
+
+//Testing the linedlist with data
+LinkedList<string> my_list = new LinkedList<string>();
+
+my_list.Add("Shalom");
+my_list.Add("Jerry");
+my_list.Add("Hope");
+
+my_list.display();
+
+Console.WriteLine(my_list.Check("Hope"));
+Console.WriteLine(my_list.Index("Gar"));
+
+
+
+
+
+
+
